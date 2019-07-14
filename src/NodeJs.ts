37cebde1833800b716e47b44c95a59b07f9b2bf7
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2019-07-14 16:36:37 +0300

    Minor tweaks and comment to JS implementation

--- src/NodeJs.ts
+++ src/NodeJs.ts
@@ -2,8 +2,8 @@ import {INode} from "./INode";
 
 export class NodeJs<K, V> implements INode<K, V> {
     public isRed: boolean = true;
-    public left: INode<K, V> | null = null;
-    public right: INode<K, V> | null = null;
+    public left: NodeJs<K, V> | null = null;
+    public right: NodeJs<K, V> | null = null;
 
     constructor(
         public readonly key: K,
--- src/NodeManagerJs.ts
+++ src/NodeManagerJs.ts
@@ -1,6 +1,9 @@
 import {INodeManager} from "./INodeManager";
 import {NodeJs} from "./NodeJs";
 
+/**
+ * Node manager implementation that can work with any data type supported in Node.js as a value
+ */
 export abstract class NodeManagerJs<K, V> implements INodeManager<K, V> {
     public root: NodeJs<K, V> | null = null;
 
